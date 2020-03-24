a469a8e60d39237edae5bdf0fe5f1e4cfa43bd81
Anubhav Dhuliya <dhuliya.a@gmail.com>
2020-03-24 13:18:45 +0530

    Project restructure

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MONGODB_URI = require('./config/mongodb');
 const mongoose = require('mongoose');
+const path = require("path");
+
+const port = process.env.PORT || 3000;
 
 //routes
 const userRoutes = require('./routes/user_routes');
 
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: false} ));
 
 app.use(userRoutes);
 
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then((client)=>{
    console.log("Connected to mongoDB");
-   app.listen(4000,()=>{
+   app.listen(port,()=>{
       console.log("Connected to the server.");
    });
 }).catch(err=>{
